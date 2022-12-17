9b91fbe4994a497f6bd1a596ff29bec8cc3bcf5c
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2022-12-17 12:49:02 -0800

    add js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
-var Weather = "APIkey": "0849b899254dd1cda16c59bb88b16cbd"
-var APIURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=0849b899254dd1cda16c59bb88b16cbd';
-var city = input.value;
+var APIkey = '0849b899254dd1cda16c59bb88b16cbd'
 
+var input = document.getElementById('search-city')
+var serchBtn = document.getElementById('search-button')
+searchBtn.addEventListner('click', SearchCity)
+
+var city = input.value;
 var city = [];
+
+function SearchCity(city) {
+    fetch ('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=0849b899254dd1cda16c59bb88b16cbd' )
+        .then((response) => response.json())
+        .then(function (data) {
+            console.log(data);
+          });
+  }
+    
+
+
+
+
+
+
