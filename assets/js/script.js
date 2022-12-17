6f4c005ea0751cf380bc1003b8e6d5d535912e78
DESKTOP-G800GM0\uriyu <uriyuri00@gmail.com>
2022-12-17 00:13:31 -0800

    html css

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var Weather = "APIkey": "0849b899254dd1cda16c59bb88b16cbd"
+var APIURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=0849b899254dd1cda16c59bb88b16cbd';
+var city = input.value;
+
+var city = [];
